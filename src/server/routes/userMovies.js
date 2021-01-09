9ce924bdc51760ef58c9e1322dbcdbdde6701f55
Jose Reyes (Beto) <josereyes55@hotmail.com>
2021-01-08 21:20:44 -0600

    adding user movies endpoint implementation

--- src/server/routes/userMovies.js
+++ src/server/routes/userMovies.js
@@ -1,11 +1,33 @@
 import express from 'express';
+import axios from 'axios';
+
+const {
+  API_URL: apiUrl,
+} = process.env;
 
 const userMoviesRouter = (app) => {
   const router = express.Router();
   app.use('/user-movies', router);
   router.post('/', async (req, res, next) => {
-
+    const { data } = req.body;
+    const { token } = req.cookies;
+    try {
+      const { data, status } = axios({
+        url: `${apiUrl}/user-movies`,
+        method: 'POST',
+        data: {
+          data,
+        },
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      res.statue(201).json(data);
+    } catch (err) {
+      next(err);
+    }
   });
+
   router.delete('/:userMovieId', async (req, res, next) => {
 
   });

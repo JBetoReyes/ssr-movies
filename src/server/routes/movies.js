ab1504b0d4a386a0fa87938d320fc9bb1019b844
Jose Reyes (Beto) <josereyes55@hotmail.com>
2021-01-12 21:05:48 -0600

    adding movies router

--- src/server/routes/movies.js
+++ src/server/routes/movies.js
@@ -1,12 +1,35 @@
 import express from 'express';
+import axios from 'axios';
+
+const {
+  API_URL: apiUrl,
+} = process.env;
 
 const moviesRouter = (app) => {
   const router = express.Router();
 
   app.use('/movies', router);
 
-  router.get('/', (req, res, next) => {
-
+  router.get('/', async (req, res, next) => {
+    try {
+      const { token } = req.cookies;
+      const { data, status } = await axios({
+        url: `${apiUrl}/movies`,
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      res.status(200).json({
+        data: data.data,
+      });
+    } catch (err) {
+      let errorToThrow = err;
+      if (err.response && err.response.data) {
+        errorToThrow = err.response.data;
+      }
+      next(errorToThrow);
+    }
   });
 };
 
